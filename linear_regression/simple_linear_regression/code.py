5859d562a77ef08705c9a49fa056e559383d31ef
DEVANSHU-KALI <devanshu9666@gmail.com>
2025-12-03 12:38:27 +0530

    updated 12

--- linear_regression/simple_linear_regression/code.py
+++ linear_regression/simple_linear_regression/code.py
@@ -1,8 +1,18 @@
+#Note: this code uses batch gradient descent.
 import numpy as np
 import matplotlib.pyplot as plt
+# for evaluations of the model
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
-X = np.array([1, 2, 3, 4, 5])       # Example: Feature (e.g., study hours)
-Y = np.array([3, 4, 2, 5, 6])       # Example: Target (e.g., test score)
+X = np.array([1.0, 1.5, 2.0, 2.5, 3.0,
+              3.5, 4.0, 4.5, 5.0, 5.5,
+              6.0, 6.5, 7.0, 7.5, 8.0,
+              8.5, 9.0, 9.5, 10.0, 10.5])          # Feature (strudied hours )
+
+Y = np.array([2.3, 3.1, 4.2, 4.8, 6.1,
+              7.0, 8.2, 9.0, 10.1, 11.2,
+              12.0, 13.5, 14.1, 15.0, 16.3,
+              17.2, 18.1, 19.0, 20.5, 21.2])       # Target (e.g., test score)
 
 #parameters
 learning_rate = 0.01     
@@ -35,7 +45,18 @@ def cost_function(Y, Y_pred):
 final_loss = cost_function(Y, m * X + c)
 print(f"Final Loss: {final_loss:.4f}")
 
+# metrices
+Y_pred=m * X + c
+print(f'MSE:{mean_squared_error(Y,Y_pred):.4f}')
+print(f'MAE:{mean_absolute_error(Y,Y_pred):.4f}')
+print(f'R_squared:{r2_score(Y,Y_pred):.4f}')
+
 #plotting the graph with the regession line
+plt.grid(True)
+plt.title('Linear regression using batch gadient descent')
+plt.xlabel('studied hours (X)')
+plt.ylabel('test score (Y)')
 plt.scatter(X, Y, color='blue', label='Data Points')
 plt.plot(X, m * X + c, color='red', label='Fitted Line')
+plt.legend()
 plt.show()
