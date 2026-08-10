2b78b8c02b256356be9d96c620b70d3cb96ef5dc
DEVANSHU-KALI <devanshu9666@gmail.com>
2026-08-10 23:14:26 +0530

    added the code explainations for the missing data handling concept

---
+++ data_preprocessing/1) missing_data_handling/code.py
@@ -0,0 +1,8 @@
+import numpy as np
+data = np.array([[2.3, np.nan], [np.nan, 3], [4, 5]])
+print("Original data:\n", data)
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(data)
+print("transformed_data:\n",imputer.transform(data))
